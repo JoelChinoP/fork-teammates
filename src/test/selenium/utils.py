c1e033805dce324e862b2554416b5b7d925d7ca0
Joel <jchinop@unsa.edu.pe>
2025-06-30 22:01:02 -0500

    Fix log_test method to correctly compare obtained and expected values

--- src/test/selenium/utils.py
+++ src/test/selenium/utils.py
@@ -23,7 +23,7 @@ class Utils:
 
   @staticmethod
   def log_test(test_code, input_data, expected, obtained, obs):
-    status = "PASSED" if obtained.contains(expected) else "FAILED"
+    status = "PASSED" if obtained == expected else "FAILED"
     print(f"::group::[{status}] TEST-{test_code.upper()}")
 
     print(f"\tInput:\n\t\t{input_data}")
