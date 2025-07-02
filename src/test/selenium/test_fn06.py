59ba09269a72abbb2b558b894cced0bb26c69480
Joel <jchinop@unsa.edu.pe>
2025-07-02 02:22:34 -0500

    Actualizar selectores de elementos en fn05.json para reflejar cambios en la estructura del DOM.

--- src/test/selenium/test_fn06.py
+++ src/test/selenium/test_fn06.py
@@ -5,3 +5,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time, json
 from utils import Utils
+
+class TestFn05:
+    def __init__(self, driver, url):
+        self.driver, self.url, self.path = driver, url, "/web/admin/search"
+        self.form_fields = {
+            "search": (By.ID, "search-box"),
+        }
+        with open("data/fn06.json") as f:
+            self.cases = json.load(f)
+
+    def go_to_form(self):
+        self.driver.get(self.url + self.path)
+        WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.ID, "search-box"))
+        )
